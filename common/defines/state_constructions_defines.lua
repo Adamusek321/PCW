a632ec10930dab0f663570409bf3ef0af8ae6332
Adamusek321 <adamus.swag@gmail.com>
2025-04-05 13:52:44 +0200

    Przywrócenie budynków z vanilli

--- common/defines/state_constructions_defines.lua
+++
@@ -1,3 +0,0 @@
-	NDefines.NBuildings.MAX_SHARED_SLOTS = 6;				-- Max slots shared by factories
-	NDefines.NBuildings.MAX_BUILDING_LEVELS = 50;			-- Max levels a building can have.
-	NDefines.NCountry.STATE_VALUE_BUILDING_SLOTS_MULT = 24;
